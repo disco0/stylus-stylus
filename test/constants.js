ca819bd701506e45e773fb6ebba3fa7448272c80
disco0 <disco0@users.noreply.github.com>
2020-11-02 17:22:09 -0800

    [test] Clear out compiled test files

--- test/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.stylusSourceFileRegex = exports.sourceDir = void 0;
-const path_1 = require("path");
-exports.sourceDir = path_1.resolve(__dirname, 'source-tests');
-exports.stylusSourceFileRegex = /^[^\.\n]+\.styl$/;
